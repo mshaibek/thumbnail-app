b1a649fc859f7c78fdb29826de9df113e7cd59c2
mshaibek <43100287+mshaibek@users.noreply.github.com>
2021-05-23 15:24:51 -0500

    remoted prints

--- CreateThumbnail.py
+++ CreateThumbnail.py
@@ -1,18 +1,7 @@
 import boto3
 from PIL import Image
 import PIL.Image
-
-print("test")     
-print("test")     
-print("test")     
-print("test")     
-print("test")     
-print("test")     
-print("test")     
-print("test")     
-print("test")     
-print("test")     
-print("test")     
+ 
 s3_client = boto3.client('s3')
      
 def resize_image(image_path, resized_path):
